cc9cce2461ae701602c21980f15d40d8acb1013b
Teguh Panjaitan <teguhpanjaitan@gmail.com>
2019-05-23 10:09:55 +0700

    First commit

---
+++ different-page-in-one.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Different Page in One
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Different Page in One
+Plugin URI:
+Description: This plugin will able to display different page in one url based on the rule which you can set on the backend
+Author: Teguh putra utama
+Version: 1.0.0
+Author URI:
+*/
